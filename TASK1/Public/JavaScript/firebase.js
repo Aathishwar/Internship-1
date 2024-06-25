51f71513a8b9d4f55ed07009445e0be6ab79938d
Aathishwar <131230490+Aathishwar@users.noreply.github.com>
2024-06-25 17:30:17 +0530

    File uploaded

--- TASK1/Public/JavaScript/firebase.js
+++ TASK1/Public/JavaScript/firebase.js
@@ -3,12 +3,8 @@
 
 let firebaseConfig = {
   
-    apiKey: "AIzaSyAzsD3BQegPwvU953j7nIFg-xPIOLZUDCo",
-    authDomain: "blogging-website-5aedb.firebaseapp.com",
-    projectId: "blogging-website-5aedb",
-    storageBucket: "blogging-website-5aedb.appspot.com",
-    messagingSenderId: "968205811424",
-    appId: "1:968205811424:web:627fd86765f1b7a5de1a85"
+    #My API Key
+  
   
 };
 
